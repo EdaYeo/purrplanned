75edb0ead985d0c41f1136ee0885cb321d25cc4b
Ng Yuen Ning <ngyuenning@u.nus.edu>
2024-01-20 22:17:17 +0800

    delete irrelevant files

--- yuenning.py
+++
@@ -1,7 +0,0 @@
-   self.canvas = Canvas(self, width=500, height=200, bd=0, highlightthickness=0)
-    self.canvas.create_rectangle(245,50,345,150, fill='white')
-
-    self.image = tk.PhotoImage(file='chess.png')
-    self.image_id = self.canvas.create_image(50,50, image=self.image)
-
-    self.canvas.move(self.image_id, 245, 100)
